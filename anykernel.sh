ebf3de2b42e5427818a1f997a1cd679d2b4de6fc
Nahuel Gómez <nahuelgomez329@gmail.com>
2025-01-21 05:04:53 -0300

    anykernel: Milestone 24.2!
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=FloppyKernel Milestone 24.1 for Ginkgo | @Flopster101
+kernel.string=FloppyKernel Milestone 24.2 for Ginkgo | @Flopster101
 do.devicecheck=1
 do.modules=0
 do.systemless=1
