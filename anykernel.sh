b06bf49c1d2b1d92176c0dbf4fcdec79923fd476
Flopster101 <nahuelgomez329@gmail.com>
2025-07-30 15:32:02 -0300

    Floppy v1.2
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=FloppyKernel v1.1 | @Flopster101
+kernel.string=FloppyKernel v1.2| @Flopster101
 do.devicecheck=1
 do.modules=0
 do.systemless=1
